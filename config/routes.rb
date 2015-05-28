cf72f3e7738c86c38bd4dfc2c081b044ade3c5e4
MarkWaldron <markewaldron@gmail.com>
2015-05-27 20:01:56 -0400

    Add act_as_votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
+    member do
+      get "like", to: "posts#upvote"
+      get "dislike", to: "post#downvote"
+    end
     resources :comments
   end
 
